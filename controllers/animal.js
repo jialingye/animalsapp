240784a83963706eb805ee98e8304bb7f20a6ff9
Jialing Ye <jialingye@Jialings-MacBook-Pro.local>
2023-04-03 21:58:43 -0500

    add ejs

--- controllers/animal.js
+++ controllers/animal.js
@@ -9,7 +9,7 @@ const animalSeed = require('../db/animalSeed')
 // async / await (explaination)
 // Create
 router.post('/', async (req, res) => {
-
+    req.body.extinct=req.body.extinct==="on"? true:false;
     const animal = await Animal.create(req.body)
     // then do this thing
     res.redirect('/animals')
@@ -18,34 +18,46 @@ router.post('/', async (req, res) => {
 // Index
 router.get('/', async (req, res) => {
     const animals = await Animal.find({})
-    res.send(animals);
+    res.render('index.ejs',{animals});
 });
 
 // Seed(explaination) 
 router.get('/seed', async (req, res) => {
     await Animal.deleteMany({})
     await Animal.create(animalSeed)
-    res.redirect('/animal');
+    res.redirect('/animals');
 });
+
+// New
+router.get('/new',(req,res)=>{
+    res.render('new.ejs')
+})
 
 // Show
 router.get('/:id', async (req, res) => {
     const animal = await Animal.findById(req.params.id)
-    res.send(animal);
+    res.render('show.ejs',{animal});
 });
+
+// Edit
+router.get('/:id/edit', async(req,res)=>{
+    const animal = await Animal.findById(req.params.id)
+    res.render('edit.ejs',{animal})
+})
 
 // Delete
 router.delete('/:id', async (req, res) => {
     const animal = await Animal.findByIdAndDelete(req.params.id)
-    res.send({success: true, animal});
+    res.redirect('/animals')
 });
 
 // Update
 router.put('/:id', async (req, res) => {
+    req.body.extinct=req.body.extinct==="on"? true:false;
     const animal = await Animal.findByIdAndUpdate(req.params.id, req.body, {
         new: true
     })
-    res.send(animal);
+    res.redirect('/animals');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { default: mongoose } = require('mongoose');
 const app = express();
 const {PORT,DATABASE_URL} = require('./config')
-
+const methodOverride = require('method-override');
 
 app.get('/', (req, res) => {
     res.send('default route')
@@ -10,6 +10,7 @@ app.get('/', (req, res) => {
 // middleware
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(methodOverride("_method"))
 
 const animalController = require('./controllers/animal');
 app.use('/animals', animalController);
